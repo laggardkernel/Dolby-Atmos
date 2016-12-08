e163757a869d05d2ce431fc985858a3f9eb2d373
laggardkernel <laggardkernel@outlook.com>
2016-12-08 18:25:49 +0800

    Lenovo A7000 Dolby Atmosv1.1.4_r6.5, resetprop improvement

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # Lenovo K3 Dolby Atmos
+
+![Dolby Atmos](https://s13.postimg.org/eq742bjv9/Logo_Dolby_Atmos_svg.png?dl=1)
+
+**Disclaim: This is a personal experiment on Magisk, adapted from ahrion's Collective Dolby Atmos Port Thread [MM+]. No interest for distribution**
+
+### Intro
+
+This is the Dolby Atmos sound effect ported from a Lenovo rom. This should work on all devices (starting from Android 4.3) since it is a software effect. It might conflict with other dolby effects you already installed. Flash the zip, it will edit (not overwrite) your audio_effects.conf.
+
+### BQ Aquaris X5 Atmos <v1.1.4>
+
+##### Download
+
+- Download: [R6.5](http://forum.xda-developers.com/devdb/project/dl/?id=13100)
+- Download: [R6.5.1 uninstall](http://forum.xda-developers.com/devdb/project/dl/?id=13553)
+- deep_buffer remover: [Download](http://forum.xda-developers.com/devdb/project/dl/?id=13950)
+
+**r6.5**:
+
+- Fixed permissions
+- Fixed issue with audio_effects.conf on some devices
+- Fixed removal of As*
+
+##### Previous Changelogs (Lenovo K3 Dolby Atmos):
+
+**R6.3**
+
+- Added permissions in installer script
+
+**R6.2**
+
+- Patch both /system/etc/audio_effects.conf AND /system/vendor/etc/audio_effects.conf to bypass those badly implemented audio mods that blindly overwrite audio_effects.conf or put an unused audio_effects.conf in /system/vendor/etc.
+
+**R6.1**
+
+- Renamed namespace
+- Renamed Ds/DsUI to As/AsUI
+- Start from a clean rom (dirty flash) when installing this since it will not cleanup the previous version !
+
+**R5**
+
+- Removed demo video, size reduced to 3 mb
+
+**R4**
+
+- Fixed compatibility for Android 4.x in Ds.apk and DsUI.apk
+- Changed uuid and name of the library so it doesnt conflict with other dolby libraries (Ds and DsUI still do but will be overwritten)
+
+**R1**
+
+- Initial release
+
+![Dolby Atmos app Interface 1](http://forum.xda-developers.com/attachment.php?attachmentid=3314480&d=1431711136) ![Dolby Atmos app Interface 1](http://forum.xda-developers.com/attachment.php?attachmentid=3317806&d=1431888014)
+
+
+#### XDA:DevDB Information
+
+**Dolby ATMOS, App for all devices (see above for details)**
+
+##### Contributors
+
+[worstenbrood](http://forum.xda-developers.com/member.php?u=981278), [ahrion](http://forum.xda-developers.com/member.php?u=5357345), [aki_007](http://forum.xda-developers.com/member.php?u=5648292)
+
+##### Version Information
+
+```
+Status: Stable
+Current Stable Version: R6.5
+Stable Release Date: 2015-06-20
+
+Created 2015-05-15
+Last Updated 2016-10-05
+```
+
+### Magisk Port Features
+
+1. deep_buffer and raw removal for more compatibility;
+2. Fix /system/vendor/etc/audio_effects.conf for OnePlus3 ROMs;
+3. Set props by resetprop, introduced in Magisk v9;
+
+### Support Link
+
+- [[MOD][ARM][4.3+] Dolby ATMOS](http://forum.xda-developers.com/android/apps-games/mod-dolby-atmos-t3109446)
+
+### Credit
+- Orignially ported by [worstenbrood@XDA](http://forum.xda-developers.com/member.php?u=981278)
+- [ahrion](http://forum.xda-developers.com/member.php?u=5357345) and [aki_007](http://forum.xda-developers.com/member.php?u=5648292) for supplying the rom
+- Magisk module by [laggardkernel@Github](https://github.com/laggardkernel)
--- common/service.sh
+++ common/service.sh
@@ -2,45 +2,69 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
+LOGFILE=/cache/magisk.log
+buildname="custom_build.prop"
+RESETPROP="/data/magisk/resetprop"
+
+log_print() {
+  echo "DolbyAtmos: $1"
+  echo "DolbyAtmos: $1" >> $LOGFILE
+  log -p i -t DolbyAtmos "$1"
+}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-# Set any prop (with trigger)
+if [ -f "$MODDIR/$buildname" ]; then
+  "$RESETPROP" --file "$MODDIR/$buildname"
+  log_print "Set props through $MODDIR/$buildname"
+elif [ -f "/magisk/tweakprop/$buildname" ]; then
+  "$RESETPROP" --file "/magisk/tweakprop/$buildname"
+  log_print "Set props through /magisk/tweakprop/$buildname"
+else
+  # Set any prop (with trigger)
+  
+  # LOW POWER AUDIO TWEAKS
+  "$RESETPROP" lpa.decode false
+  "$RESETPROP" lpa.releaselock false
+  "$RESETPROP" lpa.use-stagefright false
+  "$RESETPROP" tunnel.decode false
+  
+  # OTHER AUDIO TWEAKS
+  "$RESETPROP" audio.deep_buffer.media false
+  "$RESETPROP" tunnel.audiovideo.decode false
+  "$RESETPROP" persist.sys.media.use-awesome 1
+  "$RESETPROP" ro.audio.samplerate 48000
+  "$RESETPROP" ro.audio.pcm.samplerate 48000
+  
+  log_print "Universal audio tweaks script has run successfully $(date +"%m-%d-%Y %H:%M:%S")"
+  
+  # VENDOR SPECIFIC DOLBY ATMOS TWEAKS
+  "$RESETPROP" dmid 3067269873
+  "$RESETPROP" audio.ds1.metainfo.key 273
+  "$RESETPROP" audio.dolby.ds2.enabled true
+  "$RESETPROP" audio.dolby.ds2.hardbypass true
+  "$RESETPROP" dolby.audio.sink.info speaker
+  "$RESETPROP" dolby.ds.dialogenhancer.state on
+  "$RESETPROP" dolby.ds.graphiceq.state on
+  "$RESETPROP" dolby.ds.intelligenteq.preset ieq_on
+  "$RESETPROP" dolby.ds.intelligenteq.state on
+  "$RESETPROP" dolby.ds.profile.name Music
+  "$RESETPROP" dolby.ds.state on
+  "$RESETPROP" dolby.ds.virt.bluetooth on
+  "$RESETPROP" dolby.ds.virt.hdmi on
+  "$RESETPROP" dolby.ds.virt.headphone_port on
+  "$RESETPROP" dolby.ds.virt.internal_speaker on
+  "$RESETPROP" dolby.ds.virt.miracast on
+  "$RESETPROP" dolby.ds.virt.usb on
+  "$RESETPROP" dolby.ds.volumeleveler.state off
+  "$RESETPROP" dolby.monospeaker false
+  
+  log_print "Dolby Atmos script has run successfully $(date +"%m-%d-%Y %H:%M:%S")"
 
-# LOW POWER AUDIO TWEAKS
-/data/magisk/resetprop lpa.decode false
-/data/magisk/resetprop lpa.releaselock false
-/data/magisk/resetprop lpa.use-stagefright false
-/data/magisk/resetprop tunnel.decode false
+  log_print "Set props through servie.sh"
 
-# OTHER AUDIO TWEAKS
-/data/magisk/resetprop audio.deep_buffer.media false
-/data/magisk/resetprop tunnel.audiovideo.decode false
-/data/magisk/resetprop persist.sys.media.use-awesome 1
-/data/magisk/resetprop ro.audio.samplerate 48000
-/data/magisk/resetprop ro.audio.pcm.samplerate 48000
-
-# VENDOR SPECIFIC DOLBY ATMOS TWEAKS
-/data/magisk/resetprop dmid 3067269873
-/data/magisk/resetprop audio.ds1.metainfo.key 273
-/data/magisk/resetprop audio.dolby.ds2.enabled true
-/data/magisk/resetprop audio.dolby.ds2.hardbypass true
-/data/magisk/resetprop dolby.audio.sink.info speaker
-/data/magisk/resetprop dolby.ds.dialogenhancer.state on
-/data/magisk/resetprop dolby.ds.graphiceq.state on
-/data/magisk/resetprop dolby.ds.intelligenteq.preset ieq_on
-/data/magisk/resetprop dolby.ds.intelligenteq.state on
-/data/magisk/resetprop dolby.ds.profile.name Music
-/data/magisk/resetprop dolby.ds.state on
-/data/magisk/resetprop dolby.ds.virt.bluetooth on
-/data/magisk/resetprop dolby.ds.virt.hdmi on
-/data/magisk/resetprop dolby.ds.virt.headphone_port on
-/data/magisk/resetprop dolby.ds.virt.internal_speaker on
-/data/magisk/resetprop dolby.ds.virt.miracast on
-/data/magisk/resetprop dolby.ds.virt.usb on
-/data/magisk/resetprop dolby.ds.volumeleveler.state off
-/data/magisk/resetprop dolby.monospeaker false
+fi
 
 # Ser SELinux Permissive
 #setenforce 0
