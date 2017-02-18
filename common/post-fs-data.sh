578ee136c3a1ab3d2207fee883326fb9165d89ab
laggardkernel <laggardkernel@outlook.com>
2017-02-18 21:50:59 +0800

    update to r1.1 and script improvement
    1. ZTE Axon 7 Atmos <v2.2.1.7_r1>  updates to r1.1. 
    2. Add two more setprop entries. 
    3. Amend the setting permission function.
    4. Amend SELinux setting.
    5. README.md updates, the module is ONLY available for Magisk v9.

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,5 +6,12 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-/data/magisk/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
-/data/magisk/sepolicy-inject --live -s audioserver -t audioserver_tmpfs -c file -p read,write,execute
+#/data/magisk/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
+#/data/magisk/sepolicy-inject --live -s audioserver -t audioserver_tmpfs -c file -p read,write,execute
+
+# Preparation for Magisk v11+ with the new MagiskSU and its sepolicy
+#/data/magisk/sepolicy-inject --live "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+#"allow audioserver audioserver_tmpfs file { read write execute }"
+
+# Ser SELinux Permissive
+setenforce 0
